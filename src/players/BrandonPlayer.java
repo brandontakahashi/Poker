9e5c167c43740984e9390af10e8644f1219a4442
brandontakahashi <brandontakahashi0@gmail.com>
2024-04-09 19:56:31 -0700

    commit

--- src/players/BrandonPlayer.java
+++ src/players/BrandonPlayer.java
@@ -25,29 +25,34 @@ public class BrandonPlayer extends Player {
 
     @Override
     protected boolean shouldFold() {
-        return false;
+        // Fold if the hand is weaker than a pair or if the bet is more than 30% of the bank
+        return evaluatePlayerHand().getValue() < HandRanks.PAIR.getValue() || getGameState().getTableBet() > getBank() * 0.3;
     }
 
     @Override
     protected boolean shouldCheck() {
+        //Check if the hand is at least a high card
         return evaluatePlayerHand().getValue() >= HandRanks.HIGH_CARD.getValue();
 
     }
 
     @Override
     protected boolean shouldCall() {
-        return false;
+        // Call if the bet is not too high compared to the bank and the hand strength is decent
+        return evaluatePlayerHand().getValue() >= HandRanks.PAIR.getValue() && getGameState().getTableBet() < getBank() * 0.3; // Call if the bet is less than 30% of the bank
     }
 
     @Override
     protected boolean shouldRaise() {
+        //Raise if the hand is at least a pair and the bet is less than 10% of the bank
         boolean hand = evaluatePlayerHand().getValue() >= HandRanks.PAIR.getValue();
-        boolean bank = getGameState().getTableBet() < getBank() * 0.1;
+        boolean bank = getGameState().getTableBet()  < getBank() * 0.1;
         return hand && bank;
     }
 
     @Override
     protected boolean shouldAllIn() {
+        //Go all in if the hand is at least a four of a kind
         return evaluatePlayerHand().getValue() >= HandRanks.FOUR_OF_A_KIND.getValue();
     }
 }
